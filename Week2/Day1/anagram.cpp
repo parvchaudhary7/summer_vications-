7786a95c568147e8b44f19cf92f4d01f32347424
parvchaudhary7 <parvchaudhary7000@gmail.com>
2026-06-11 21:55:14 +0530

    day 1 is here

---
+++ Week2/Day1/anagram.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool isAnagram(string s, string t) {
+
+    if(s.length() != t.length())
+        return false;
+
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+
+    return s == t;
+}
+
+int main() {
+
+    string s = "anagram";
+    string t = "nagaram";
+
+    if(isAnagram(s, t))
+        cout << "True";
+    else
+        cout << "False";
+
+    return 0;
+}
